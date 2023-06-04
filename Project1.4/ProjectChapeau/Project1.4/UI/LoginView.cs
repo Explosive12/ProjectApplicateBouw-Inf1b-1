a2238d89b5c92e39d4f11bae8c2ad0d75a0bcf46
NickS721 <Nickschaap304@gmail.com>
2023-06-04 21:35:57 +0200

    Login/Table class diagram

---
+++ Project1.4/ProjectChapeau/Project1.4/UI/LoginView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1._4
+{
+    public partial class LoginView : Form
+    {
+
+        public string GetUsername()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetPassword()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LoginScreen()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ManagerView()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WaitressView()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BartenderView()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ChefView()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
