170cb8620cf2cbaa90a9c55807ff3e6d44acc435
Gecko <kian.steffes@gmail.com>
2023-06-07 22:27:48 +0200

    Added Product, OrderItem and Order DAO

--- Project1.4/ProjectChapeau/Project1.4/DAL/ProductDao.cs
+++ Project1.4/ProjectChapeau/Project1.4/DAL/ProductDao.cs
@@ -5,39 +5,45 @@ using System.Linq;
 using System.Text;
 using static System.Windows.Forms.LinkLabel;
 using Project1._4.Model;
+using System.Data.SqlClient;
 
 namespace Project1._4.DAL
 {
     public class ProductDao : BaseDao
     {
-        // get all items from table
         public List<Product> GetAllProducts()
         {
-            //string query = "";
-            //SqlParameter[] sqlParameters = new SqlParameter[0];
-            //return ReadTables(ExecuteSelectQuery(query, sqlParameters));
-            return new List<Product>(); // remove this, this is just to prevent errors
+            string query = "SELECT productId, naam, prijs, voorraad, btw FROM product";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        public List<Product> GetByIdProduct(int productId)
+        {
+            string query = "SELECT productId, naam, prijs, voorraad, btw FROM product WHERE productId = @productId";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@productId", productId);
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
 
         private List<Product> ReadTables(DataTable dataTable)
         {
             List<Product> products = new List<Product>();
 
-            //foreach (DataRow dr in dataTable.Rows)
-            //{
-            //    Product product = new Product(
-            //        );
-            //    products.Add(product);
-            //}
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Product product = new Product(
+                    (int)dr["productId"],
+                    (string)dr["naam"],
+                    (decimal)dr["prijs"],
+                    (int)dr["voorraad"],
+                    (decimal)dr["btw"]
+                    );
+                products.Add(product);
+            }
             return products;
         }
 
-        public List<Product> GetByIdProduct(int productId)
-        {
-            //string query = "";
-            //SqlParameter[] sqlParameters = new SqlParameter[0];
-            //return ReadTablesSupervisor(ExecuteSelectQuery(query, sqlParameters));
-            return new List<Product>(); // remove this, this is just to prevent errors
-        }
+        
     }
 }
--- Project1.4/ProjectChapeau/Project1.4/Model/Product.cs
+++ Project1.4/ProjectChapeau/Project1.4/Model/Product.cs
@@ -7,9 +7,18 @@ namespace Project1._4.Model
 {
     public class Product
     {
+        public Product(int ProductId, string Name, decimal Price, int Stock, decimal Btw)
+        {
+            this.ProductId = ProductId;
+            this.Name = Name;
+            this.Price = Price;
+            this.Stock = Stock;
+            this.Btw = Btw;
+        }
         public int ProductId { get; set; }
         public string Name { get; set; }
-        public int Voorraad { get; set; }
-        public decimal Prijs { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public decimal Btw { get; set; }
     }
 }
