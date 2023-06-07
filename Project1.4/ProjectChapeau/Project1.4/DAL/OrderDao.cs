a0113c79df1d8571a67feac54b06fc9ffb3a4ee3
rangodepizza <700836@student.inholland.nl>
2023-06-07 14:37:04 +0200

    Update OrderDao.cs
    updated the methods and querry's and the interaction with the database

--- Project1.4/ProjectChapeau/Project1.4/DAL/OrderDao.cs
+++ Project1.4/ProjectChapeau/Project1.4/DAL/OrderDao.cs
@@ -8,22 +8,23 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Project1._4.Model;
 using Microsoft.Graph;
+using Microsoft.Kiota.Abstractions;
+using System.Windows.Forms;
 
 namespace Project1._4.DAL
 {
     public class OrderDao : BaseDao
     {
         //connection string for the database
-        const string connectionString = "Data Source=someren2023db.database.windows.net; Initial Catalog=someren_db; User=somerendb; Password=kaleharrie3895!";
+        const string connectionString = "Data Source=applicatiebouw.database.windows.net; Initial Catalog = databaseproject1.4; User=nrtjk14; Password=Kipsate14";
+
         // get all items from table
         public List<Order> GetAllOrders()
         {
             //Activiteit gegevens uit de database halen.
-
-            //TODO querry aanpassen voor de juiste data
-            string query = "SELECT activiteitID, activiteitNaam, startDatum, eindDatum " +
-                            "FROM Activiteit " +
-                            "ORDER BY activiteitID";
+            string query =  "SELECT P.naam, BR.aantal, BR.opmerking " +
+                            "FROM bestelregel AS BR " +
+                                "JOIN product AS P ON P.productId = BR.productId";
             SqlParameter[] parameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, parameters));
         }
@@ -43,31 +44,61 @@ namespace Project1._4.DAL
 
         public List<Order> GetByIdOrder(int orderId)
         {
-            ///TODO eddit querry to mach names from project 1.4
+            //Activiteit gegevens uit de database halen.
+
+            //TODO change query to mach the objective of the method
+            string query =  "SELECT P.naam, BR.aantal, BR.opmerking " +
+                            "FROM bestelregel AS BR " +
+                                "JOIN product AS P ON P.productId = BR.productId " +
+                            $"WHERE bestellingId = {orderId} " +
+                            "ORDER BY P.naam";
+            SqlParameter[] parameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, parameters));
+        }
+
+
+        public void InsertOrder(int tafelId, int beginTijd, int aantal, string opmerking)
+        {
             //Define the SQL query to insert a new order with the given parameters
-            string query = "INSERT INTO [Activiteit_deelnemers] (activiteitID, studentID) " +
-                            "VALUES (@activiteitID, @studentID)";
+            string query =  "INSERT INTO bestelling (tafelId, begintijd)" +
+                            "VALUES(@tafelID, @begintijd)";
 
             //Create a new SQL connection and command with the above query
             SqlConnection connection = new SqlConnection(connectionString);
             SqlCommand command = new SqlCommand(query, connection);
 
             //Add parameters to the command to set the values of the activity ID, student ID
-            command.Parameters.AddWithValue("@activiteitID", Activity);
-            command.Parameters.AddWithValue("@studentID", StudentID);
+            command.Parameters.AddWithValue("@tafelID", tafelId);
+            command.Parameters.AddWithValue("@begintijd", beginTijd);
 
             //Open the database connection, execute the query, and close the connection
             connection.Open();
-            int rowsAffected = command.ExecuteNonQuery();
+            int firstTimeRowsAffected = command.ExecuteNonQuery();
             connection.Close();
 
-            //string query = "";
-            //SqlParameter[] sqlParameters = new SqlParameter[0];
-            //return ReadTablesSupervisor(ExecuteSelectQuery(query, sqlParameters));
-            return new List<Order>(); // remove this, this is just to prevent errors
-        }
 
-        public void InsertOrder(Order order) { }
+
+
+
+            //Define the SQL query to insert a new order with the given parameters
+            query = "INSERT INTO bestelregel (aantal, opmerking) " +
+                    "VALUES (@aantal, @opmerking)";
+
+            //Create a new SQL connection and command with the above query
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand(query, connection);
+
+            //Add parameters to the command to set the values of the activity ID, student ID
+            command.Parameters.AddWithValue("@aantal", aantal);
+            command.Parameters.AddWithValue("@opmerking", opmerking);
+
+            //Open the database connection, execute the query, and close the connection
+            connection.Open();
+            int seconnedTimeRowsAffected = command.ExecuteNonQuery();
+            connection.Close();
+
+            int totalRowsAffected = firstTimeRowsAffected + seconnedTimeRowsAffected;
+        }
         public void RemoveOrder(Order order) { }
         public void UpdateOrder(Order order) { }
     }
