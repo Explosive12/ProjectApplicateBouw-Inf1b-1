c64147b5672c625be87cce84825837c49b25ecfa
thijszoontjes <74897795+thijszoontjes@users.noreply.github.com>
2023-06-08 13:44:01 +0200

    update

--- Project1.4/ProjectChapeau/Project1.4/DAL/PaymentDao.cs
+++ Project1.4/ProjectChapeau/Project1.4/DAL/PaymentDao.cs
@@ -8,13 +8,15 @@ namespace Project1._4.DAL
 {
     internal class PaymentDao
     {
-        public void GetOrders() { }
+        /*public void GetOrders() { }
         
         public void GetTableNumber() { } 
 
         public void GetTipPercent() { }
 
         public void GetTipPrice() { }
-        public void GetTotalPrice() { }
+        public void GetTotalPrice() { }*/
+
+      
     }
 }
