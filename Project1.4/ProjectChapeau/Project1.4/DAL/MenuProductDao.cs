82532db3537ad37f7b7bccef155ae6a2a6b0653e
Explosive12 <102148117+Explosive12@users.noreply.github.com>
2023-06-08 13:29:49 +0200

    Added menuProductDao (it sucks)

--- Project1.4/ProjectChapeau/Project1.4/DAL/MenuProductDao.cs
+++ Project1.4/ProjectChapeau/Project1.4/DAL/MenuProductDao.cs
@@ -11,45 +11,67 @@ namespace Project1._4.DAL
 {
     public class MenuProductDao : BaseDao
     {
-        private List<Menu> ReadTables(DataTable dataTable)
+        private List<MenuProduct> ReadTables(DataTable dataTable)
         {
-            List<Menu> menu = new List<Menu>();
+            List<MenuProduct> menu = new List<MenuProduct>();
 
-            //foreach (DataRow dr in dataTable.Rows)
-            //{
-            //    Order order = new Order(
-            //        );
-            //    orders.Add(product);
-            //}
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                MenuProduct menuItem = new MenuProduct((int)dr[0], (int)dr[1]);
+                menu.Add(menuItem);
+            }
             return menu;
         }
         
-
+        
         public List<MenuProduct> GetAllMenuItems()
         {
             //Menu gegevens uit de database halen.
-            string query = "SELECT * FROM Menu";
+            string query = "SELECT productId, menuId FROM MenuProducts";
             SqlParameter[] parameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, parameters));
+
         }
 
         public MenuProduct GetMenuItemById(int id)
         {
-            return null;
+            //Menu gegevens uit de database halen via id.
+            string query = "SELECT productId, menuId FROM MenuProducts WHERE menuId = @id";
+            SqlParameter[] parameters = new SqlParameter[1];
+            parameters[0] = new SqlParameter("@id", id);
+            return ReadTables(ExecuteSelectQuery(query, parameters))[0];
         }
         
         public void AddMenuItem(MenuProduct menu)
         {
+            // Menu gegevens toevoegen aan de database.
+
+            string query = "INSERT INTO MenuProducts (productId, menuId) VALUES (@productId, @menuId)";
+            SqlParameter[] parameters = new SqlParameter[2];
+            parameters[0] = new SqlParameter("@productId", menu.ProductId);
+            parameters[1] = new SqlParameter("@menuId", menu.MenuId);
+            ExecuteEditQuery(query, parameters);
 
         }
 
         public void UpdateMenuItem(MenuProduct menu)
         {
+            // Menu gegevens updaten in de database.
+            string query = "UPDATE MenuProducts SET productId = @productId, menuId = @menuId WHERE menuId = @menuId";
+            SqlParameter[] parameters = new SqlParameter[2];
+            parameters[0] = new SqlParameter("@productId", menu.ProductId);
+            parameters[1] = new SqlParameter("@menuId", menu.MenuId);
+            ExecuteEditQuery(query, parameters);
 
         }
 
         public void DeleteMenuItem(MenuProduct menu)
         {
+            // Menu gegevens verwijderen uit de database.
+            string query = "DELETE FROM MenuProducts WHERE menuId = @menuId";
+            SqlParameter[] parameters = new SqlParameter[1];
+            parameters[0] = new SqlParameter("@menuId", menu.MenuId);
+            ExecuteEditQuery(query, parameters);
 
         }
     }
