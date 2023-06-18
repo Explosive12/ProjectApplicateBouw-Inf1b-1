23e3f6fa4005e756d119ef6d0dfbc3b784c883b0
NickS721 <113018071+NickS721@users.noreply.github.com>
2023-06-18 21:18:41 +0200

    dev nick

--- Project1.4/ProjectChapeau/Project1.4/UI/LoginView.cs
+++ Project1.4/ProjectChapeau/Project1.4/UI/LoginView.cs
@@ -9,6 +9,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
